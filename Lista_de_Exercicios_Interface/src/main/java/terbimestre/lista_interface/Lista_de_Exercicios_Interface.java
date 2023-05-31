764684ee87dc32a1dc87a366b1efa14dcfff3619
Alan Henrique Ribeiro <93848010+DelGrau@users.noreply.github.com>
2023-05-30 21:02:11 -0300

    Update Lista_de_Exercicios_Interface.java
    removido Imports desnecessários

--- Lista_de_Exercicios_Interface/src/main/java/terbimestre/lista_interface/Lista_de_Exercicios_Interface.java
+++ Lista_de_Exercicios_Interface/src/main/java/terbimestre/lista_interface/Lista_de_Exercicios_Interface.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package terbimestre.lista_interface;
 import terbimestre.lista_interface.ex1.ex1;
 import terbimestre.lista_interface.ex2.ex2;
 import terbimestre.lista_interface.ex3.ex3;
 import terbimestre.lista_interface.ex4.ex4;
-import terbimestre.lista_interface.ex5.ex5;
-import terbimestre.lista_interface.ex6.ex6;
-import terbimestre.lista_interface.ex7.ex7;
-import terbimestre.lista_interface.ex8.ex8;
-import terbimestre.lista_interface.ex9.ex9;
-import terbimestre.lista_interface.ex10.ex10;
 
 /**
  *
